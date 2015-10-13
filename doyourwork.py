0a37cf7104345dc84e18611a9b0598750138e256
Sanket Rathi <sanket@coverfoxmail.com>
2015-10-13 19:04:32 +0530

    First commit

---
+++ doyourwork.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+import argparse
+import subprocess
+import time
+
+
+def main(argv=None):
+    argv = argv if argv is not None else sys.argv[1:]
+    parser = argparse.ArgumentParser(description='Protects you from procrastinating.')
+    parser.add_argument('--workspace', help='Workspace where you are working', default='1')
+    parser.add_argument('--time', help='Time you are allowed to stay outside workspace, in seconds', type=int, default=60)
+    parser.add_argument('--sleep', help='Sleep time between workspace checking, in seconds', type=int, default=10)
+
+    args = parser.parse_args()
+    watch_workspace = args.workspace
+    allowed_tp_time = args.time
+    sleep_time = args.sleep
+    time_since_last_change = 0
+
+    cmd = ['xprop', '-root', '-notype', '_NET_CURRENT_DESKTOP']
+    while True:
+        x = subprocess.check_output(cmd).strip('\n').split(' = ')[-1]
+        if x is not watch_workspace:
+            time_since_last_change += sleep_time
+        else:
+            time_since_last_change = 0
+        if time_since_last_change >= allowed_tp_time:
+            subprocess.call(['notify-send', 'FUCKING GET BACK TO WORK!'])
+            time_since_last_change = 0
+        time.sleep(sleep_time)
+
+if __name__ == '__main__':
+    exit(main())
